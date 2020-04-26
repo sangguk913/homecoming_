bd59a39d69d13b565d2213d5cdcf0595c8b001ab
sangguk913 <64362269+sangguk913@users.noreply.github.com>
2020-04-26 22:58:34 +0900

    Update ch3_assignment.py

---
+++ ch3_assignment.py
@@ -0,0 +1,36 @@
+#1330
+A, B = map(int, input().split(' '))
+if A > B:
+    print('>')
+elif A < B:
+    print('<')
+elif A == B:
+    print('==')
+
+#9498
+score = int(input())
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+elif score >= 60:
+    print('D')
+else:
+    print('F')
+
+#2753
+Y = input()
+Y = int(Y)
+if Y%4==0:
+    if Y%100!=0:
+        print('1')
+    elif Y%400==0:
+        print('1')
+    else:
+        print('0')
+else:
+    print('0')
+
+#14681

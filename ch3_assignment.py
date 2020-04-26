3857a4ca31b97fdc39ac0908f8e946fa2d179c8f
sangguk913 <64362269+sangguk913@users.noreply.github.com>
2020-04-26 23:16:11 +0900

    Update ch3_assignment.py

--- ch3_assignment.py
+++ ch3_assignment.py
@@ -34,3 +34,13 @@ else:
     print('0')
 
 #14681
+x=int(input())
+y=int(input())
+if x>0 and y>0:
+    print('1')
+elif x<0 and y>0:
+    print('2')
+elif x<0 and y<0:
+    print('3')
+else:
+    print('4')
